0a3d3b72430dbcbb597714069d0277883c9a9761
Aarya Ahjolia <aaryaahjolia282@gmail.com>
2024-10-17 12:16:56 +0530

    Fixed Carousel Scroll

--- src/Carousel.js
+++ src/Carousel.js
@@ -27,23 +27,26 @@ function Carousel_Main() {
     const carouselRef = useRef();
     let totalSlides = 3;
     const [centerSlide, setCenterSlide] = useState(0);
-
+    let hoverTimeout;
     const handleMouseEnter = (index) => {
         // alert(centerSlide + " " + index);
+        clearTimeout(hoverTimeout);
+        hoverTimeout = setTimeout(() => {
+            if ((centerSlide + 1) % totalSlides === index) {
+                const nextSlide = carouselRef.current.state.currentSlide + 1;
+                carouselRef.current.goToSlide(nextSlide);
+                setCenterSlide(index);
+            } else if ((centerSlide - 1 + totalSlides) % totalSlides === index) {
+                const prevSlide = carouselRef.current.state.currentSlide - 1;
+                carouselRef.current.goToSlide(prevSlide);
+                setCenterSlide(index);
+            }
+        }, 200); // Adjust the delay as needed
+    };
 
-        if ((centerSlide + 1) % totalSlides === index) {
-            const nextSlide = carouselRef.current.state.currentSlide + 1;
-            carouselRef.current.goToSlide(nextSlide);
-            setCenterSlide(index);
-            return;
-        }
-        if ((centerSlide - 1) % totalSlides === index || (centerSlide === 0 && index !== 0)) {
-            const nextSlide = carouselRef.current.state.currentSlide - 1;
-            carouselRef.current.goToSlide(nextSlide);
-            setCenterSlide(index);
-            return;
-        }
-        
+    const handleMouseLeave = () => {
+        // Clear the timeout when mouse leaves
+        clearTimeout(hoverTimeout);
     };
 
     return (
@@ -56,21 +59,21 @@ function Carousel_Main() {
                 removeArrowOnDeviceType={["desktop", "tablet", "mobile"]}
                 draggable={false}
             >
-                <div className='custom_carousel p-10' onMouseEnter={() => handleMouseEnter(0)}>
+                <div className='custom_carousel p-10' onMouseEnter={() => handleMouseEnter(0)} onMouseLeave={handleMouseLeave}>
                     <div className='w-full h-96 mt-10'>
                         <img src={Carousel_1} alt='Carousel 1' className='w-full h-full object-cover rounded-2xl' />
                     </div>
                     <h1 className='text-center uppercase text-3xl mt-16'>Doritos after dark (NYC)</h1>
                 </div>
 
-                <div className='custom_carousel p-10' onMouseEnter={() => handleMouseEnter(1)}>
+                <div className='custom_carousel p-10' onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={handleMouseLeave}>
                     <div className='w-full h-96 mt-10'>
                         <img src={Carousel_2} alt='Carousel 2' className='w-full h-full object-cover rounded-2xl' />
                     </div>
                     <h1 className='text-center uppercase text-3xl mt-16'>Hot Topic Room (YVR)</h1>
                 </div>
 
-                <div className='custom_carousel p-10' onMouseEnter={() => handleMouseEnter(2)}>
+                <div className='custom_carousel p-10' onMouseEnter={() => handleMouseEnter(2)} onMouseLeave={handleMouseLeave}>
                     <div className='w-full h-96 mt-10'>
                         <img src={Carousel_3} alt='Carousel 3' className='w-full h-full object-cover rounded-2xl' />
                     </div>
